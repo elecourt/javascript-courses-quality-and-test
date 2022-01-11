7e78a8041c458d78195f4ddf2e4fa057ac955e6f
Damien Dabernat <damien.dabernat@gmail.com>
2022-01-11 12:52:50 +0100

    Add express

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const Todos = require('./todos.js');
 
-let todos = new Todos();
+/*let todos = new Todos();
 todos.ask().then(answers => {
   todos.add(answers['name']);
   console.log(todos.list());
-});
+});*/
+
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send("cool"))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
